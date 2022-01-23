841dbff7a3a94b3c619612fe77bc7b2a0c84c509
Sammeeey <samuel.netherlands@gmx.net>
2022-01-23 08:28:37 +0100

    amend createAd view amend views
    amend createAd view amend views.createAd so that it's at least able to display either the empty form (if it's called by a get method) to be filled in with data or - if it's called with a post method - it can at least create a new model with dummy strings (see newAd variable in views.createAd).

--- codermatching/codermatch/views.py
+++ codermatching/codermatch/views.py
@@ -70,7 +70,18 @@ def createAd(request):
     """
     This view method enables to publish own individual ads on the page.
     """
-    return render(request, 'codermatch/createAd.html')
+    #if it's a post request: create a new object by posting the form data & display the created ad
+    if request.method == 'POST':
+        newAd = Ad(projectTitle='project-title',
+                    creatorName='creator-name',
+                    projectDescription='project-description',
+                    contactDetails='contact-details')
+        newAd.save()
+        return HttpResponseRedirect(reverse('codermatch:adDetail', args=(newAd.id,)))
+    #if it's a get request: show the form
+    else: #if request.method == 'GET'
+        return render(request, 'codermatch/createAd.html')
+
 
 def adSearch(request):
     return HttpResponse('This function should should view a search where people could sort and search for ads with certain properties...')
