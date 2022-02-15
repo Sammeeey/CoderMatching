846b2d7cf319d4499b7caa7c7543aede80d23779
Sammeeey <samuel.netherlands@gmx.net>
2022-02-15 07:38:36 +0100

    chore: delete non-project content from project
    - remove testTemplating.html
    - remove not used urlpatterns

--- codermatching/codermatch/urls.py
+++ codermatching/codermatch/urls.py
@@ -8,7 +8,4 @@ urlpatterns = [
     path('ad-search/', views.adSearch, name='adSearch'),
     path('ad-detail/<int:adId>/', views.detail, name='adDetail'),
     path('create-ad/', views.createAd, name='createAd'),
-    # path('test-templating/', views.testTemplating, name='testTemplating'),
-    # path('test-templating<int:numArg>/', views.testTemplating, name='testTemplating'),
-    # path('<int:adId>/vote/', views.vote, name='vote'),
 ]
