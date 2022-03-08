0820dcd578131d2ad8afdb220513f5ccaab14dbe
Sammeeey <samuel.netherlands@gmx.net>
2022-03-08 08:29:04 +0100

    chore: add dummyfile to staticfiles directory
    - add dummyfile here: codermatching\staticfiles\dummyFile.py
    - settings.py: add path for STATIC_ROOT
    - based on: https://devcenter.heroku.com/articles/django-assets
    	- to fit the static files settings and setup, which Heroku expects: staticfiles directory (with dummyfile) in root + STATIC_ROOT defined to use respective directory

---
+++ staticfiles/dummyFile.py
@@ -0,0 +1,4 @@
+# This is a dummyfile for deployment on Heroku.
+# See https://devcenter.heroku.com/articles/django-assets
+# It says:
+# Django won’t automatically create the target directory (STATIC_ROOT) that collectstatic uses, if it isn’t available. You may need to create this directory in your codebase, so it will be available when collectstatic is run. Git does not support empty file directories, so you will have to create a file inside that directory as well.
