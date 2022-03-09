5cf0ef18f8a2f58b0578dabdcea909819ca6497c
Sammeeey <samuel.netherlands@gmx.net>
2022-03-09 10:11:33 +0100

    docs: add assumption referring collection of static files
    - add assumption in dummyFile.py
    	- actually only meant to be a trivial code change (compared to the remote counterpart on Heroku)
    	- to avoid heroku/git saying *Everything up-to-date* when running `git push heroku testDeploy:main`
    	- to enable re-deploying testDeploy branch to heroku with config var DISABLE_COLLECTSTATIC unset
    		- expected behavoir: heroku/django-heroku/whitenoise collects staticfiles automatically when running `git push heroku testDeploy:main`
    		- if instead heroku complains about *Error while running '$ python codermatching/manage.py collectstatic --noinput'.* -> re-activate DISABLE_COLLECTSTATIC=1 and try to collect static files in remote repository on heroku (using heroku bash(?))

--- staticfiles/dummyFile.py
+++ staticfiles/dummyFile.py
@@ -2,3 +2,6 @@
 # See https://devcenter.heroku.com/articles/django-assets
 # It says:
 # Django won’t automatically create the target directory (STATIC_ROOT) that collectstatic uses, if it isn’t available. You may need to create this directory in your codebase, so it will be available when collectstatic is run. Git does not support empty file directories, so you will have to create a file inside that directory as well.
+
+# assumption by Sam:
+# You probably don't need the dummyfile anymore once you collect static files in advance (locally, before deployment) or after cloning a repository
