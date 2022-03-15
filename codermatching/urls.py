94ff9828c8be61182a9918f574bf7851b1fdeee3
Sammeeey <samuel.netherlands@gmx.net>
2022-03-15 13:49:00 +0100

    feat: Use codermatch app URLs without prefix
    urls.py
    - remove codermatch/ from path
    
    reason:
    That way the URLs from the codermatch app should be used normally.
    So we don't need to call the domain-name like https://codermatching.herokuapp.com/codermatch/ to access the index, but we should be able to access it by calling https://codermatching.herokuapp.com/

--- codermatching/urls.py
+++ codermatching/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('codermatch/', include('codermatch.urls')),
+    path('', include('codermatch.urls')),
     path('admin/', admin.site.urls),
 ]
