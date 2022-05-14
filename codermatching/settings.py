bac6c1f0b3c9b3626478785cd33ced5686a05bab
Samuel Hartmann <hartmann.samuel@gmx.de>
2022-05-14 11:14:13 +0200

    style: design landing page based on figma template
    - based on [Figma template](https://www.figma.com/file/HsIHYoX3FksUVcWfBWv18h/CoderMatching-LandingPage)
    
    use font-family Nunito for body
    
    refactor: move header image into nav
    
    feat: base body max width: 90%
    
    style: remove 'How to' image from index
    
    chore: enable debug mode
    
    - set DEBUG = True in settings.py
    	- to use Django's automatic collectstatic functionality in debug mode
    
    style: center body
    
    style: set maximum size for header logo
    
    style: design main heading
    
    style.css
    - import google fonts (Fredoka & Courgette)
    	- replace Nunito font by Fredoka
    
    base.html
    - replace previous heading with h1
    	- including two seperate p elements with newly imported fonts (see above)
    
    edit main button style
    
    base.html
    - change button text
    
    style.css
    - change button style based on figma template
    
    add and style benefits section of index
    
    style: add minimal css reset
    
    add minimal css reset rules in style.css to ensure uniformity among different browsers
    
    style: position main 'post ad' button again
    
    - after adding css reset properties in previous commit
    
    style: add 'find collab' button in benefits section
    
    style: change h2 (and its content) appearance
    
    - to change size and font-weight of 'Most recent Classifieds' heading on landing page
    
    style: edit button borders
    
    - to have the same line-width on hover and 'not-hover'
    	- to avoid content on landing page from jumping up and down on hover (as if non-hover state had no border)
    
    increase main heading size on landing page
    
    style: add margin above header logo (landing page)

--- codermatching/settings.py
+++ codermatching/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SEC_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.localhost', '127.0.0.1']
 
